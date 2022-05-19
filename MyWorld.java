890e2a986bf6c44b9d79fa4d56d52f861b8e9801
Marian-Lin <350862694@gapps.yrdsb.ca>
2022-05-18 22:02:42 -0400

    added a falling banana and movement

--- MyWorld.java
+++ MyWorld.java
@@ -17,5 +17,11 @@ public class MyWorld extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
+        
+        elephant e = new elephant();
+        addObject(e,300,350);
+        
+        banana a = new banana();
+        addObject(a,200,25);
     }
 }
