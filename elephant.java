5f6288d967bd7991d719deb35810b9912ff9b485
Marian-Lin <350862694@gapps.yrdsb.ca>
2022-05-25 21:09:55 -0400

    added background and sound

--- elephant.java
+++ elephant.java
@@ -12,6 +12,8 @@ public class Elephant extends Actor
      * Act - do whatever the elephant wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    GreenfootSound elephantSound = new GreenfootSound("elephant5.mp3");
+    
     public void act()
     {
         // Add your action code here.
@@ -30,6 +32,7 @@ public class Elephant extends Actor
             MyWorld world = (MyWorld)getWorld();
             world.increaseScore();
             world.spawnBanana();
+            elephantSound.play();
         }
     }
 }
