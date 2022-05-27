f0cbfcc65f97164bc0345598c13fdd1876346ef9
Marian-Lin <350862694@gapps.yrdsb.ca>
2022-05-27 12:08:22 -0400

    added timer

--- elephant.java
+++ elephant.java
@@ -13,18 +13,32 @@ public class Elephant extends Actor
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     GreenfootSound elephantSound = new GreenfootSound("elephant5.mp3");
-    GreenfootImage [] idle = new GreenfootImage[8];
+    GreenfootImage [] idlea = new GreenfootImage[8];
+    GreenfootImage [] idled = new GreenfootImage[8];
     
+    //Direction the elephant is facing
+    String facing = "a";
+    SimpleTimer animtionTimer = new SimepaleTimer
     /**
      * Constructor - The code that gets run one time when object is created
      */
     public Elephant()
     {
-        for (int i = 0;i<idle.length;i++)
+        for (int i = 0;i<idlea.length;i++)
         {
-            idle[i] = new GreenfootImage("images/idle" + i +".png");
+            idlea[i] = new GreenfootImage("images/idle" + i +".png");
+            idlea[i].scale(100,100);
         }
-        setImage(idle[0]);
+        for(int i = 0;i <idled.length;i++)
+        {
+            idled[i] = new GreenfootImage("images/idle" + i +".png");
+            idled[i].mirrorHorizontally();
+            idled[i].scale(100,100);
+        }
+        
+        animationTimer.mark();
+        //Initial elephant image
+        setImage(idlea[0]);
     }
     
     /*
@@ -32,20 +46,35 @@ public class Elephant extends Actor
      */
     int imageIndex = 0;
     public void animateElephant()
-    {
-        setImage(idle[imageIndex]);
-        imageIndex = (imageIndex + 1) % idle.length;
-    }
+    {   
+        if(animationTimer.millisElapsed()<100)
+        {
+            return;
+        }
+        animationTimer.mark();
+        if(facing.equals("a"))
+        {
+            setImage(idlea[imageIndex]);
+            imageIndex = (imageIndex + 1) % idlea.length;
+        }
+        else
+        {
+            setImage(idled[imageIndex]);
+            imageIndex = (imageIndex + 1) % idled.length;
+        }
+    }    
     public void act()
     {
         // Add your action code here.
         if(Greenfoot.isKeyDown("d"))
         {
             move(3);
+            facing = "a";
         }
         if(Greenfoot.isKeyDown("a"))
         {
             move(-3);
+            facing = "d";
         }   
         //Remove banana if elephant eats it
         
